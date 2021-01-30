482a5ea9f4a0f106e230014a31adae908ca6d38d
asiyeyildirim <asiyeeyildirim71@gmail.com>
2021-01-30 19:52:34 +0300

    Car Menu

--- dosya1/dosya1/main.py
+++ dosya1/dosya1/main.py
@@ -54,7 +54,7 @@ while True:
             main_menu=True
             print(".............")
         elif choice==4:
-            bike.returnVehicle(customer.returnVehicle("bike"),"bike")
+            customer.bill= bike.returnVehicle(customer.returnVehicle("bike"),"bike")
             customer.rentalBasis_b, customer.rentalTime_b,customer.bikes=0,0,0
             main_menu=True
         elif choice==5:
@@ -63,7 +63,63 @@ while True:
             break
         else:
             print("Invalid input.Please enter a number between 1-6")
+            main_menu=True
+    elif choice=="B" or choice=="b":
+       print(""" 
+        ****CAR MENU***
+        1.Display available cars
+        2.Request a car on hourly basis $10
+        3.Request a car on daily basis $192
+        4.return a car
+        5.Main Menu
+        6.Exit
+     """)
 
+       choice=input("Enter choice:")
+
+       try:
+          choice=int(choice)
+       except ValueError:
+            print("It is not integer")
+            continue
+
+       if choice==1:
+            car.displayStock()
+            choice="B"
+
+       elif choice==2:
+            customer.rentalTime_c=car.rentHourly(customer.requestVehicle("car"))
+            customer:rentalBasis_c=1
+            main_menu=True
+            print(".............")
+
+       elif choice==3:
+            customer.rentalTime_c=car.rentDaily(customer.requestVehicle("car"))
+            customer:rentalBasis_c=2
+            main_menu=True
+            print(".............")
+       elif choice==4:
+            customer.bill=  bike.returnVehicle(customer.returnVehicle("bike"),"bike")
+            customer.rentalBasis_c, customer.rentalTime_c,customer.cars=0,0,0
+            main_menu=True
+       elif choice==5:
+            main_menu=True
+       elif choice==6:
+            break
+       else:
+            print("Invalid input.Please enter a number between 1-6")
+            main_menu=True
+
+
+    elif choice=="Q" or choice=="q": 
+        break
+
+    else:
+        print("Invalid input.Please Enter A_B-Q")
+        main_menu=True
+    print( "Thank you for using the vehicle rental shop")
+
+ 
 
 
  
